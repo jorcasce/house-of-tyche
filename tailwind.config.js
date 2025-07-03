b15de8bb2febf0ab2840756969b9b6a4ea957abf
jorcaser <jorcasce@gmail.com>
2025-07-03 17:25:50 -0600

    test#

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/* eslint-env node */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
