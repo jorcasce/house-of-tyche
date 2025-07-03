715d7864e3c49ec4bd8b6e71c5ed2cf68d382f54
jorcaser <jorcasce@gmail.com>
2025-07-03 16:24:32 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,16 @@ function App() {
         <Route path="/" element={<div className="p-6 text-white text-2xl">Welcome to House of Tyche</div>} />
         {/* <Route path="/casinos" element={<Casinos />} />*/}
         <Route path="/casinos" element={<Layout><Casinos /></Layout>} />
-        <Route path="/casinos/casino1" element={<Casino1 />} />
+        {/* <Route path="/casinos/casino1" element={<Casino1 />} />
+        
         <Route path="/casinos/casino2" element={<Casino2 />} />
         <Route path="/casinos/casino3" element={<Casino3 />} />
-        <Route path="/casinos/casino4" element={<Casino4 />} />
+        <Route path="/casinos/casino4" element={<Casino4 />} />*/}
+        <Route path="/casinos" element={<Layout><Casino1 /></Layout>} />
+        <Route path="/casinos" element={<Layout><Casino2 /></Layout>} />
+        <Route path="/casinos" element={<Layout><Casino3 /></Layout>} />
+        <Route path="/casinos" element={<Layout><Casino4 /></Layout>} />
+
         {/* <Route path="/contact" element={<Contact />} /> */}
         <Route path="/contact" element={<Layout><Contact /></Layout>} />
         {/*<Route path="/about" element={<About />} /> */}
